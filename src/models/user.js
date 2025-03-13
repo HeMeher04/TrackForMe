576ba10e8a65a663559af4f3f37698eddec0b47d
HeMeher04 <hemsagarmeher786@gmail.com>
2025-03-13 23:17:02 +0530

    Adding user schema

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+
+const userSchema= new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minlength:3,
+        maxlength:30
+    },
+    lastName:{
+        type:String,
+        minlength:3,
+        maxlength:30
+    },
+    emailId:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email");
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Invalid Password");
+            }
+        }
+    },
+},{timestamps : true});
+
+const user= mongoose.model("User",userSchema);
+module.exports = user;
