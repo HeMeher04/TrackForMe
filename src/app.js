c813909e67e2af4dba3c42f8c83631885b090c8f
HeMeher04 <hemsagarmeher786@gmail.com>
2025-03-13 23:03:54 +0530

    Connecing to MongoDB

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+const connectDB = async(req,res) =>{
+    await mongoose.connect(`${process.env.URI}/${process.env.DB_Name}`);
+}
+connectDB()
+.then(()=>{
+    console.log("Connected to MongoDB");
+    app.listen(3333,()=>{
+        console.log("Server is running on port 3333");
+    })
+})
+.catch((err)=>{
+    console.log(err);
+    process.exit(1);
+})
